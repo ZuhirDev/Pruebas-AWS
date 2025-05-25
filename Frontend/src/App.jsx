17e57149812eab5915adcd25d0ab0e6bcb320695
ZuhirDev <zelhaouzyamrani@gmail.com>
2025-05-25 20:04:21 +0200

    new App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,29 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react';
 
 const App = () => {
   const [message, setMessage] = useState('');
+  const [showMessage, setShowMessage] = useState(false);
 
-  useEffect(() => {
+  const handleClick = () => {
     fetch('/api/hello')
-    .then((response) => response.text())
-    .then((data) => setMessage(data));
-  }, []);
+      .then((response) => response.text())
+      .then((data) => {
+        setMessage(data);
+        setShowMessage(true);
+        setTimeout(() => {
+          setShowMessage(false);
+        }, 3000);
+      });
+  };
+
   return (
     <div>
       <h1>Frontend</h1>
       <p>Esta aplicación se conecta al backend pidiéndole una respuesta</p>
-      <p>Backend response: {message}</p>
+      <button onClick={handleClick}>Pedir mensaje al backend</button>
+      {showMessage && <p>Backend response: {message}</p>}
     </div>
   );
-}
+};
 
-export default App
+export default App;
